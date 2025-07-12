0f3f29e510739ee13f7358adc5a5a9593372200a
bvh@scriptor.pro <bvh@scriptor.pro>
2025-07-12 15:10:35 +0200

    tags etc.

--- content/tags.md
+++ content/tags.md
@@ -3,3 +3,4 @@ title: "Liste des Tags"
 description: "Tous les tags utilisés dans ce jardin numérique"
 type: "tags"
 ---
+# Tags
--- quartz.config.ts
+++ quartz.config.ts
@@ -38,7 +38,7 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#005fa3",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
