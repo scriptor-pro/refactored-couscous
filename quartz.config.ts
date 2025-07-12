445683b4572b7572f9e5d2bf3a18067b8164d2ea
bvh@scriptor.pro <bvh@scriptor.pro>
2025-07-12 14:58:03 +0200

    couleur du fond

--- quartz.config.ts
+++ quartz.config.ts
@@ -33,7 +33,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#fff8f8",
+          light: "#fdfdfd",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
